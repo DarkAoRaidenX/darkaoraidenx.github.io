e44fa68fd894c2cb53b0e783e6d693e97639be6e
DarkAoRaidenX <101914106+DarkAoRaidenX@users.noreply.github.com>
2023-10-28 18:29:37 -0700

    Things

---
+++ drg/tips/scripts/jquery/tabs-drg.js
@@ -0,0 +1,51 @@
+$( function() {
+   $( "#tabs" ).tabs();
+ } );
+
+ $( function() {
+   $( "#general-tips" ).tabs();
+ } );
+
+ $( function() {
+   $( "#dwarves-tips" ).tabs();
+ } );
+
+ $( function() {
+   $( "#dwarf-driller" ).tabs();
+ } );
+
+ $( function() {
+   $( "#dwarf-engineer" ).tabs();
+ } );
+
+ $( function() {
+   $( "#dwarf-gunner" ).tabs();
+ } );
+
+ $( function() {
+   $( "#dwarf-scout" ).tabs();
+ } );
+
+ $( function() {
+   $( "#driller-equipment" ).tabs();
+ } );
+
+ $( function() {
+   $( "#engineer-equipment" ).tabs();
+ } );
+
+ $( function() {
+   $( "#gunner-equipment" ).tabs();
+ } );
+
+ $( function() {
+   $( "#scout-equipment" ).tabs();
+ } );
+
+ $( function() {
+   $( "#mission" ).tabs();
+ } );
+
+ $( function() {
+   $( "#machine-events" ).tabs();
+ } );
