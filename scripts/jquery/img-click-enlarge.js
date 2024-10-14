edbc32ab1c5b1e17128c52e41d12826597b080ea
DarkAoRaidenX <101914106+DarkAoRaidenX@users.noreply.github.com>
2024-10-14 12:43:01 -0700

    Things i forgot already

---
+++ scripts/jquery/img-click-enlarge.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.img-example').on('click', function(){
+    $(this).toggleClass('img-example-enlarge');
+});
+});
+
+// $(document).ready(function() {
+//   $("img").click(function(){
+//     $(".img-example").toggleClass("img-example-enlarge");
+//   });
+// });
