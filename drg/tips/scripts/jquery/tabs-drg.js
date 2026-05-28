86e718678199c4deeb8cdc71c84480b9976806e9
DarkAoRaidenX <101914106+DarkAoRaidenX@users.noreply.github.com>
2026-05-27 17:08:13 -0700

    Individual Main Pages+
    Updated Mods, Rehaul Navs and Dwarves

--- drg/tips/scripts/jquery/tabs-drg.js
+++ drg/tips/scripts/jquery/tabs-drg.js
@@ -1,68 +1,249 @@
 $( function() {
-   $( "#tabs" ).tabs();
- } );
-
- $( function() {
-   $( "#general-tips" ).tabs();
- } );
-
-$( function() {
-  $( "#gameplay-tips" ).tabs();
+  $( "#general-tips-content" ).tabs();
 } );
 
 $( function() {
-  $( "#resources-tips" ).tabs();
+$( "#gameplay-tips" ).tabs();
 } );
 
 $( function() {
-  $( "#minerals-tips" ).tabs();
+$( "#resources-tips" ).tabs();
 } );
 
 $( function() {
-  $( "#buried-gems-tips" ).tabs();
+$( "#minerals-tips" ).tabs();
+} );
+
+$( function() {
+$( "#buried-gems-tips" ).tabs();
 } );
 
 
- $( function() {
-   $( "#dwarves-tips" ).tabs();
- } );
+$( function() {
+  $( "#dwarves-tips" ).tabs();
+} );
 
- $( function() {
-   $( "#dwarf-driller-tips-content" ).tabs();
- } );
+$( function() {
+  $( "#dwarf-driller-tips-content" ).tabs();
+} );
 
- $( function() {
-   $( "#dwarf-engineer-tips-content" ).tabs();
- } );
+$( function() {
+  $( "#dwarf-engineer-tips-content" ).tabs();
+} );
 
- $( function() {
-   $( "#dwarf-gunner-tips-content" ).tabs();
- } );
+$( function() {
+  $( "#dwarf-gunner-tips-content" ).tabs();
+} );
 
- $( function() {
-   $( "#dwarf-scout-tips-content" ).tabs();
- } );
+$( function() {
+  $( "#dwarf-scout-tips-content" ).tabs();
+} );
 
- $( function() {
-   $( "#driller-equipment" ).tabs();
- } );
 
- $( function() {
-   $( "#engineer-equipment" ).tabs();
- } );
+// Driller
+$( function() {
+  $( "#nav-main-driller-loadouts-content" ).tabs();
+} );
 
- $( function() {
-   $( "#gunner-equipment" ).tabs();
- } );
+// Driller Primary
+$( function() {
+  $( "#driller-primary-loadouts" ).tabs();
+} );
 
- $( function() {
-   $( "#scout-equipment" ).tabs();
- } );
+$( function() {
+  $( "#nav-driller-cyro-tips-content" ).tabs();
+} );
 
- $( function() {
-   $( "#mission-tips" ).tabs();
- } );
+$( function() {
+  $( "#nav-driller-flamethrower-tips-content" ).tabs();
+} );
 
- $( function() {
-   $( "#machine-events" ).tabs();
- } );
+$( function() {
+  $( "#nav-driller-sludge-pump-tips-content" ).tabs();
+} );
+
+// Driller Secondary
+$( function() {
+  $( "#driller-secondary-loadouts" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-driller-epc-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-driller-subata-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-driller-wave-cooker-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#driller-equipment-content" ).tabs();
+} );
+
+$( function() {
+  $( "#driller-throwables-content" ).tabs();
+} );
+
+// Engineer
+$( function() {
+  $( "#nav-main-engineer-loadouts-content" ).tabs();
+} );
+
+// Engineer Primary
+$( function() {
+  $( "#engineer-primary-loadouts" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-engineer-auto-shotgun-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-engineer-smart-rifle-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-engineer-stubby-smg-tips-content" ).tabs();
+} );
+
+// Engineer Secondary
+$( function() {
+  $( "#engineer-secondary-loadouts" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-engineer-breach-cutter-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-engineer-deepcore-40mm-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-engineer-shard-diffractor-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#engineer-equipment-content" ).tabs();
+} );
+
+$( function() {
+  $( "#engineer-throwables-content" ).tabs();
+} );
+
+
+// Gunner
+$( function() {
+  $( "#nav-main-gunner-loadouts-content" ).tabs();
+} );
+
+// Gunner Primary
+$( function() {
+  $( "#gunner-primary-loadouts" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-gunner-autocannon-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-gunner-hurricane-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-gunner-minigun-tips-content" ).tabs();
+} );
+
+// Gunner Secondary
+$( function() {
+  $( "#gunner-secondary-loadouts" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-gunner-brt7-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-gunner-bulldog-revolver-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-gunner-coil-gun-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#gunner-equipment-content" ).tabs();
+} );
+
+$( function() {
+  $( "#gunner-throwables-content" ).tabs();
+} );
+
+
+// Scout
+$( function() {
+  $( "#nav-main-scout-loadouts-content" ).tabs();
+} );
+
+// Scout Primary
+$( function() {
+  $( "#scout-primary-loadouts" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-scout-deepcore-gk2-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-scout-drak-25-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-scout-m1000-tips-content" ).tabs();
+} );
+
+// Scout Secondary
+$( function() {
+  $( "#scout-secondary-loadouts" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-scout-boltshark-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-scout-boomstick-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#nav-scout-zhukov-tips-content" ).tabs();
+} );
+
+$( function() {
+  $( "#scout-equipment-content" ).tabs();
+} );
+
+$( function() {
+  $( "#scout-throwables-content" ).tabs();
+} );
+
+
+
+
+
+
+
+
+
+
+
+$( function() {
+  $( "#mission-tips" ).tabs();
+} );
+
+$( function() {
+  $( "#machine-events" ).tabs();
+} );
